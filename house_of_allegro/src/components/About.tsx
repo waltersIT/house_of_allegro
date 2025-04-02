6949d7d3c3b11099db5b9b8372afb827a924329c
waltersIT <walter@walterit.net>
2025-04-02 11:16:46 -0400

    Update About.tsx

--- house_of_allegro/src/components/About.tsx
+++ house_of_allegro/src/components/About.tsx
@@ -1,47 +1,56 @@
 import NavBar from "./styles/NavBar";
 import Footer from "./styles/Footer";
-import info from './info.json';
-import Alec from './styles/assets/Alec.jpg';
-import Live from './styles/assets/Live.jpg';
-import DJ from './styles/assets/DJ.jpg';
-import Table from './styles/assets/Table.jpg';
+import info from "./info.json";
+import Alec from "./styles/assets/Alec.jpg";
+import Live from "./styles/assets/Live.jpg";
+import DJ from "./styles/assets/DJ.jpg";
+import Table from "./styles/assets/Table.jpg";
 
 interface Props {
-    title: string,
-    descriptionOne: string,
-    descriptionTwo: string,
-    descriptionThree: string,
-    descriptionFour: string
+  title: string;
+  descriptionOne: string;
+  descriptionTwo: string;
+  descriptionThree: string;
+  descriptionFour: string;
 }
 
-function About({title, descriptionOne, descriptionTwo, descriptionThree, descriptionFour}: Props) {
-    return(
-        <>
-        <NavBar title={info.title}/>
-        <div className="section" style={{ paddingTop: '30px' }}>
-            <div className="gallery">
-            <img src={Alec} />
-            <img src={DJ} />
-            <img src={Table} />
-            <img src={Live} />
+function About({
+  title,
+  descriptionOne,
+  descriptionTwo,
+  descriptionThree,
+  descriptionFour,
+}: Props) {
+  return (
+    <>
+      <NavBar title={info.title} />
+      <div className="section" style={{ paddingTop: "30px" }}>
+        <div className="gallery">
+          <img src={Alec} />
+          <img src={DJ} />
+          <img src={Table} />
+          <img src={Live} />
         </div>
+      </div>
+      <div className="mobile-spacer" />
+      <div className="section">
+        <div className="calendar-box">
+          <div className="events-container">
+            <h2 className="calendar-title">
+              {title}
+              <div className="line"></div>
+            </h2>
+            <p>{descriptionOne}</p>
+            <p>{descriptionTwo}</p>
+            <p>{descriptionThree}</p>
+            <p>{descriptionFour}</p>
+          </div>
         </div>
-            <div className="section">
-                <div className="about-box">
-                    <div className="events-container">
-                    <h2 className="calendar-title">
-                        {title}
-                        <div className="line"></div>
-                    </h2>
-                    <p>{descriptionOne}</p>
-                    <p>{descriptionTwo}</p>
-                    <p>{descriptionThree}</p>
-                    <p>{descriptionFour}</p>
-                </div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+      </div>
+      <div className="mobile-spacer" />
+
+      <Footer />
+    </>
+  );
 }
 export default About;
