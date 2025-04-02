ed37ecef96140f26c09bff1943d9169ece5d1fa3
waltersIT <walter@walterit.net>
2025-04-02 11:17:01 -0400

    Update Calendar.tsx

--- house_of_allegro/src/components/Calendar.tsx
+++ house_of_allegro/src/components/Calendar.tsx
@@ -1,26 +1,20 @@
-
 import NavBar from "./styles/NavBar";
 import info from "./info.json";
 import Footer from "./styles/Footer";
 import Events from "./styles/Events";
 import PassedEvents from "./PassedEvents";
 
-
 interface Props {
   title: string;
 }
 
 function GoogleCalendar({ title }: Props) {
-
-
-
-
   return (
     <>
-
       <NavBar title={title} />
-        <Events title={info.calendar.title} />
-        <PassedEvents title={info.calendar.passedTitle} />
+      <Events title={info.calendar.title} />
+      <div className="mobile-spacer" />
+      <PassedEvents title={info.calendar.passedTitle} />
       <Footer />
     </>
   );
